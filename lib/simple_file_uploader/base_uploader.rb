e562f6d1a9234e3c5df98098502e5e7e02021de4
Gallyamov Insaf <gallyamov.insaf@gmail.com>
2016-07-25 11:53:42 +0300

    add find_uploader_by_uuid method

--- lib/simple_file_uploader/base_uploader.rb
+++ lib/simple_file_uploader/base_uploader.rb
@@ -1,6 +1,7 @@
 module SimpleFileUploader
   class UploaderError < SimpleFileUploaderError; end
   class BaseUploader
+
     attr_accessor :model, :mounted_as, :column_value, :errors
 
     def initialize(model, mounted_as)
@@ -23,6 +24,15 @@ module SimpleFileUploader
       def remember_uploader(model_class, column)
         @mounted_uploaders ||= []
         @mounted_uploaders << {model_class: model_class.name, column: column}
+      end
+
+      def find_uploader_by_uuid(uuid)
+        mounted_uploaders.each do |mounted_uploader|
+          clazz = mounted_uploader[:model_class].constantize
+          found_model = clazz.where(:"#{mounted_uploader[:column]}" => uuid).take
+          return found_model.send(:"#{mounted_uploader[:column]}") if found_model.present?
+        end if mounted_uploaders.present?
+        nil
       end
     end
 
